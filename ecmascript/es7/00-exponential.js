bc8f9ca81253aaf36a580e439c9faba1da294a5c
Alejandro Dubon <alejandrodubon88@gmail.com>
2022-10-11 21:19:41 -0600

    feat: es7 examples was finished

---
+++ ecmascript/es7/00-exponential.js
@@ -0,0 +1,4 @@
+const number = 2 ** 3;
+console.log(number);
+
+// Note: we already have the object math with the method pow
---
+++ ecmascript/es7/01-includes.js
@@ -0,0 +1,15 @@
+// 🔗 En objetos también existen formas para saber si existe una propiedad. Estos son:
+
+// La palabra reservada in
+// El método de objetos hasOwnProperty
+// El método Object.hasOwn, que recibe el objeto y la propiedad a evaluar.
+
+const words = { a: 1, b: 2, c: 3 }
+const numbers = [1, 2, 3, 4, 5]
+
+
+console.log(numbers.includes(3))
+
+"a" in words // true
+words.hasOwnProperty("a") // true
+Object.hasOwn(words, "a") // true
